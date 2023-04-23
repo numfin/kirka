67d331a66f208f2d2a954a05cb5d26dd55eeb162
a coder <a coder>
2023-04-23 22:44:29 +0300

    Pipe for chain functions

--- src/pipe/index.ts
+++ src/pipe/index.ts
@@ -1,20 +1,23 @@
 export interface Pipe<In, Out> {
   chain<NewOut>(member: (v: Out) => NewOut): Pipe<In, NewOut>;
   call(v: In): Out;
+  clone(): Pipe<In, Out>;
 }
 
-export function Pipe<In, Out = In>(identity?: (v: In) => Out) {
-  const members: Function[] = [];
+export function Pipe<In, Out = In>(
+  identity = (v: In) => v as unknown as Out,
+  members = [] as Function[]
+) {
   const pipe: Pipe<In, Out> = {
     call(v) {
-      return members.reduce(
-        (lastV, member) => member(lastV),
-        (identity?.(v) ?? v) as Out
-      );
+      return members.reduce((lastV, member) => member(lastV), identity(v));
     },
     chain<NewOut>(member: (v: Out) => NewOut) {
       members.push(member);
       return pipe as unknown as Pipe<In, NewOut>;
+    },
+    clone() {
+      return Pipe(identity, Array.from(members));
     },
   };
   return pipe as Pipe<In, Out>;
---
+++ src/pipe/pipe.test.ts
@@ -0,0 +1,18 @@
+import test from "ava";
+import { Pipe } from "./index.js";
+
+test("Can chain functions", (t) => {
+  const pipe = Pipe((v: string) => v + "asd").chain((v) => v + "qwe");
+  t.is(pipe.call("hi"), "hiasdqwe");
+});
+test(".chain() mutates original pipe", (t) => {
+  const pipe = Pipe((v: string) => v + "asd").chain((v) => v + "qwe");
+  pipe.chain((v) => v + "zxc");
+  t.is(pipe.call("hi"), "hiasdqwezxc");
+});
+test(".clone()", (t) => {
+  const pipe = Pipe((v: string) => v + "asd").chain((v) => v + "qwe");
+  const pipe2 = pipe.clone().chain((v) => v + "zxc");
+  t.is(pipe.call("hi"), "hiasdqwe");
+  t.is(pipe2.call("hi"), "hiasdqwezxc");
+});
